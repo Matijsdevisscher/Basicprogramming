51321a1c213a8fc023b2952d65a21392c746001c
matijs <matijs.devisscher@student.howest.Be>
2017-09-26 09:28:45 +0200

    initial commit

---
+++ Oef01.py
@@ -0,0 +1,11 @@
+# Dit is commentaar (commentarieer je programma)
+# Labo_Intro Oef01
+
+naam = input("Wat is jouw naam?")
+voornaam = "Matys"
+leeftijd= int(input("Hoe jong ben jij?"))
+leeftijd = leeftijd + 10
+#naam = "dev"
+#print("Welkom!" , voornaam, "\t", naam )
+
+print( "Welkom (0). Jouw naam is (1). \njij bent (2:.2f) jaar.".format(voonaam, naam, "iets"))
