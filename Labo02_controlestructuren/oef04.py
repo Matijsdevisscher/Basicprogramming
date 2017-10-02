d8307ba9b02798fd8cf2711a6a88f66200d65d65
matijs <matijs.devisscher@student.howest.Be>
2017-10-02 12:04:41 +0200

    oefening 04

---
+++ Labo02_controlestructuren/oef04.py
@@ -0,0 +1,17 @@
+#oefening 04
+
+getal1 = int(input("Geef de leeftijd op van u hond: "))
+
+if (getal1 < 0):
+    print("Error ")
+elif(getal1 == 1):
+    print("Deze leeftijd komt overeen met 14 mensenjaren")
+elif(getal1 == 2):
+    print("Deze leeftijd komt overeen met 22 mensenjaren")
+else:
+    resultaat = 22 + getal1 + 3*5
+    print(resultaat)
+
+
+
+
