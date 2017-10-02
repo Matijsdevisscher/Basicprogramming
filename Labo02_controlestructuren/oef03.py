d377a877c1c6ed972d9e95c438cd93c03985e920
matijs <matijs.devisscher@student.howest.Be>
2017-10-02 11:46:58 +0200

    oefening 03

---
+++ Labo02_controlestructuren/oef03.py
@@ -0,0 +1,8 @@
+
+
+geboortejaar = int(input("Wat is uw geboortejaar? "))
+
+if (geboortejaar < 1998):
+    print("Ok, u mag alcohol drinken")
+else:
+    print("U bent nog geen 18! Kom volgend jaar terug")
