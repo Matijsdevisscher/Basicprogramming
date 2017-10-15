7713d629eb27332ddb863944c3ea9ef0a993032d
matijs <matijs.devisscher@student.howest.Be>
2017-10-15 12:22:15 +0200

    initial commit (oefenen)

--- Labo02_controlestructuren/Oef11week02.py
+++ Labo02_controlestructuren/Oef11week02.py
@@ -1,12 +1,10 @@
 
 
-def geef_celcius (getalcelsius):
-    temp = (t_in_fahrenheit - 32) * 5 / 9
-    print(temp)
+def celcius (T_in_fahrenheit):
+    return (T_in_fahrenheit - 32) * 5 / 9
 
-def geef_fahrenheit (getalfahrenheit):
-    temp = (temp_in_celsius * 9 / 5) + 32
-    print(temp)
+def fahrenheit (T_in_celsius):
+    return (T_in_celsius * 9 / 5) + 32
 
 eenheid = int(input("Welke eenheid gebruikt u? C: Celsius, F: Fahrenheit  "))
 
@@ -19,4 +17,6 @@ elif(eenheid == F):
 else:
     print("ERROR: Geef de eerste letter op van de gebruikte eenheid Celsius of Fahrenheit")
 
+eenheid = input("Welke eenheid gebruikt u? [C: celsius, F: fahrenheit] ")
+opgegeven_eenheid = "celsius" if (eenheid == "C")
 
